2fa4dd63de91b0d9cab5b091d3c665562738cd0c
wmmask <wf1029@post.com>
2020-07-31 22:30:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _5_4_Lab_Creating_Mario
+{
+    class Program
+    {
+        public struct Mario // Structure for the Mario Character
+        {
+            public enum Size
+            {
+                small, 
+                large
+            }
+
+            public int numOfLives;
+            public string currentLevel;
+            public Size sizeOfMario;
+            public bool canShootFireBalls;
+            public bool isJumping;
+            public int numOfCoins;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Our Mario with default values
+            Mario mario = new Mario();
+            mario.numOfLives = 3;
+            mario.currentLevel = "World 1-1";
+            mario.sizeOfMario = Mario.Size.small;
+            mario.canShootFireBall = false;
+            mario.isJumping = false;
+            mario.numOfCoins = 25;
+
+
+
+    }
+    }
+}
